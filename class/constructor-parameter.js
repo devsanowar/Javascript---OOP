a82595c63c195b865c1744950a64c5d6e385a588
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-07-27 08:13:44 +0600

    learn js constructor with practice

---
+++ class/constructor-parameter.js
@@ -0,0 +1,11 @@
+/**
+ * কনস্ট্রাকটর প্যারামিটার কিভাবে কাজ করে
+ */
+
+class Person{
+    constructor(num1, num2){
+        console.log(num1 + num2);
+    }
+}
+
+let personObj = new Person(20, 30);
